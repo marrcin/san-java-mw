ba278dcc01aafd41381bf6fe7902be7fc1f9c955
marrcin <marcinkutno>
2013-05-11 09:51:17 +0200

    Projekt losowania

--- src/pl/juglodz/losowanie/Generator.java
+++ src/pl/juglodz/losowanie/Generator.java
@@ -2,33 +2,62 @@ package pl.juglodz.losowanie;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import javax.annotation.Generated;
 
 public class Generator {
-	
-	public List<String> generateImiona(int number){
-	
-	List<String> imiona = new ArrayList<String>();
-	imiona.add("Marcin");
-	imiona.add("Agnieszka");
-	imiona.add("Ala");
-	imiona.add("Ola");
-	imiona.add("Kazio");
-	
-	return imiona;
-	
+
+	public List<String> generateImiona(int number) {
+
+		List<String> imiona = new ArrayList<String>();
+		imiona.add("Marcin");
+		imiona.add("Agnieszka");
+		imiona.add("Ala");
+		imiona.add("Ola");
+		imiona.add("Kazio");
+
+		return imiona;
+
+	}
+
+	public List<String> generateNazwiska(int number) {
+		List<String> nazwiska = new ArrayList<String>();
+		nazwiska.add("Kowalski");
+		nazwiska.add("Nowak");
+		nazwiska.add("Ryś");
+
+		return nazwiska;
+
+	}
+
+	private Random losowacz = new Random();
+
+	public List<Osoba> generetaOsoba(int number) {
+		List<Osoba> osoby = new ArrayList<Osoba>();
+		List<String> imiona = generateImiona(number);
+		List<String> nazwiska = generateNazwiska(number);
+		for (int i = 0; i < number; i++) {
+			Osoba o = new Osoba();
+			o.setImie(imiona.get(losowacz.nextInt(imiona.size())));
+			o.setNazwisko(nazwiska.get(losowacz.nextInt(nazwiska.size())));
+			
+			osoby.add(o);
+		}
+		
+
+		return osoby;
+
 	}
 	
-	public List<String> generateNazwiska(int number){
-	List<String> nazwiska = new ArrayList<String>();
-	nazwiska.add("Kowalski");
-	nazwiska.add("Nowak");
-	nazwiska.add("Ryś");
-	
-	return nazwiska;
+	public static void main(String args[]){
+		Generator gr = new Generator();
+		
+		List<Osoba> osoby = gr.generetaOsoba(666);
+		for (Osoba osoba : osoby) {
+		System.out.println(osoba);	
+		}
 	
 	}
-	
-	
-	
 
 }
---
+++ src/pl/juglodz/losowanie/Osoba.java
@@ -0,0 +1,25 @@
+package pl.juglodz.losowanie;
+
+public class Osoba {
+	private String imie;
+	private String nazwisko;
+	public String getImie() {
+		return imie;
+	}
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+	public String getNazwisko() {
+		return nazwisko;
+	}
+	public void setNazwisko(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+	@Override
+	public String toString() {
+		return "Osoba [imie=" + imie + ", nazwisko=" + nazwisko + "]";
+	}
+	
+	
+
+}
